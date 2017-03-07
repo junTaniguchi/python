231f7c8ca2a98764ae14d199ac800da0822c8c8d
junTaniguchi <junTaniguchi@users.noreply.github.com>
2017-03-07 15:12:44 +0900

    Update and rename learn_charset.py to learning_charset.py

--- learning_charset.py
+++ learning_charset.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  5 08:43:20 2017
+Created on Mon Mar  6 13:05:42 2017
 
-@author: JunTaniguchi
+@author: j13-taniguchi
 """
 
 from keras.models import Sequential
+import tensorflow
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD, Adam, RMSprop
@@ -21,30 +22,33 @@ image_h = 28
 
 def main():
     # フォント画像のデータを読む
-    xy = np.load("/Users/JunTaniguchi/Desktop/pythonPlayGround/study_tensorflow/keras_project/image/font_draw.npz")
+    xy = np.load("/Users/j13-taniguchi/study_tensorflow/keras_project/image/japanese_dataset.npz")
     X = xy["x"]
     Y = xy["y"]
     # データを正規化
-    X = X.reshape(X.shape[0], image_w * image_h).astype('float32')
+    #X = X.reshape(X.shape[0], image_w * image_h).astype('float32')
     X /= 255
     # ラベル作成
-    Y = np_utils.to_categorical(Y, 2180)
-    with open("/Users/JunTaniguchi/Desktop/pythonPlayGround/study_tensorflow/keras_project/japanese_lang/japanese_lang.txt") as japanese_file:
+    Y = np_utils.to_categorical(Y, 2195)
+    with open("/Users/j13-taniguchi/study_tensorflow/keras_project/japanese_lang.txt") as japanese_file:
         japanese_lang = japanese_file.read()
         japanese_cat = len(japanese_lang)
     # 訓練データとテストデータに分割
     X_train, X_test, y_train, y_test = \
         cross_validation.train_test_split(X, Y)
+    X_train = np.reshape(X_train, (len(X_train),  28, 28, 1))
+    X_test  = np.reshape(X_test, (len(X_test), 28, 28, 1))
     print('X_train shape:', X_train.shape)
     # モデルを構築
     model = build_model(japanese_cat)
+    model.summary()
     model.fit(X_train, y_train,
         batch_size=128, nb_epoch=10, verbose=1,
         validation_data=(X_test, y_test))
     # モデルを保存
-    model.save_weights('/Users/JunTaniguchi/Desktop/pythonPlayGround/study_tensorflow/keras_project/font_draw.hdf5')
+    model.save_weights('/Users/j13-taniguchi/study_tensorflow/keras_project/japanese_dataset.hdf5')
     model_json = model.to_json()
-    with open('/Users/JunTaniguchi/Desktop/pythonPlayGround/study_tensorflow/keras_project/lang.json', 'w') as json_file:
+    with open('/Users/j13-taniguchi/study_tensorflow/keras_project/japanese_dataset.json', 'w') as json_file:
         json.dump(model_json, json_file, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
     
     # モデルを評価
@@ -53,7 +57,7 @@ def main():
 
 def build_model(japanese_cat):
     # MLPのモデルを構築
-    '''
+    '''単純全結合モデル
     model = Sequential()
     model = Sequential()
     model.add(Dense(512, input_shape=(image_w * image_h,)))
@@ -65,27 +69,53 @@ def build_model(japanese_cat):
     model.add(Dense(japanese_cat))
     model.add(Activation('softmax'))
     '''
+    '''CNN
     model = Sequential()
-#    model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(44347, image_w, image_h)))
-    model.add(Convolution2D(32, 3, 3, 
-	border_mode='same',
-	input_shape=X_train.shape[1:]))
+    model.add(Convolution2D(32, 1, 3, border_mode='valid', input_shape=(0, 1, image_w * image_h)))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, 1, 3, border_mode='valid'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(1, 2)))
     model.add(Dropout(0.25))
     
-    model.add(Convolution2D(64, 3, 3, border_mode='same'))
+    model.add(Convolution2D(32, 1, 3, border_mode='valid'))
     model.add(Activation('relu'))
-    model.add(Convolution2D(64, 3, 3))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(32, 1, 3, border_mode='valid'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(1, 2)))
     model.add(Dropout(0.25))
     
     model.add(Flatten())
     model.add(Dense(512))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(japanese_cat))
+    model.add(Dense(2195))
     model.add(Activation('softmax'))
+    '''
+
+    #VGG
+    model = Sequential()
+    model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(image_w, image_h, 1)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2195))
+    model.add(Activation('softmax'))
+
 
     model.compile(loss='categorical_crossentropy',
         optimizer=RMSprop(),
@@ -94,4 +124,3 @@ def build_model(japanese_cat):
 
 if __name__ == '__main__':
     main()
-
