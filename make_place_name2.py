a9d30d18ec85e52d957b2a76d620ea201f61f0ce
junTaniguchi <junTaniguchi@users.noreply.github.com>
2017-03-10 01:05:49 +0900

    Update make_place_name2.py

--- make_place_name2.py
+++ make_place_name2.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageDraw, ImageFont
 import cv2
 import numpy as np
 
-path = "/Users/j13-taniguchi/study_tensorflow/keras_project/read_place"
+path = "/Users/JunTaniguchi/study_tensorflow/keras_project/read_place"
 os.chdir(path)
 
 #フォンントのリストを作成
@@ -33,7 +33,7 @@ for idx1, jp_font in enumerate(jp_font_list):
 
     for idx2, place_name in enumerate(place_list):
         # 画像のピクセルを指定
-        image = Image.new('L', (300, 300))
+        image = Image.new('RGB', (300, 300), 'blue')
         draw = ImageDraw.Draw(image)
         # 日本語の文字を入れてみる
         # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
@@ -48,15 +48,15 @@ for idx1, jp_font in enumerate(jp_font_list):
 for idx1, jp_font in enumerate(jp_font_list):
     # フォントの指定。引数は順に「フォントのパス」「フォントのサイズ」「エンコード」
     # メソッド名からも分かるようにTure Typeのフォントを指定する
-    font = ImageFont.truetype(jp_font, 50)
+    font = ImageFont.truetype(jp_font, 20)
 
     for idx2, place_name in enumerate(place_list):
         # 画像のピクセルを指定
-        image = Image.new('L', (300, 300))
+        image = Image.new('RGB', (300, 300), 'blue')
         draw = ImageDraw.Draw(image)
         # 日本語の文字を入れてみる
         # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
-        draw.text((140, 150), place_name, font=font, fill='#ffffff')
+        draw.text((25, 45), place_name, font=font, fill='#ffffff')
         # opencvのガウシアンフィルターを適応
         image_np = np.asarray(image)
         blur = cv2.GaussianBlur(image_np, (5, 5), 0)
@@ -72,15 +72,15 @@ for idx1, jp_font in enumerate(jp_font_list):
 for idx1, jp_font in enumerate(jp_font_list):
     # フォントの指定。引数は順に「フォントのパス」「フォントのサイズ」「エンコード」
     # メソッド名からも分かるようにTure Typeのフォントを指定する
-    font = ImageFont.truetype(jp_font, 40)
+    font = ImageFont.truetype(jp_font, 20)
 
     for idx2, place_name in enumerate(place_list):
         # 画像のピクセルを指定
-        image = Image.new('L', (300, 300))
+        image = Image.new('RGB', (300, 300), 'blue')
         draw = ImageDraw.Draw(image)
         # 日本語の文字を入れてみる
         # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
-        draw.text((100, 150), place_name, font=font, fill='#ffffff')
+        draw.text((25, 45), place_name, font=font, fill='#ffffff')
         # opencvのガウシアンフィルターを適応
         image_np = np.asarray(image)
         blur = cv2.GaussianBlur(image_np, (5, 5), 0)
@@ -91,7 +91,7 @@ for idx1, jp_font in enumerate(jp_font_list):
         size = tuple(np.array([blur.shape[1], blur.shape[0]]))
         # 回転させたい角度
         # ラジアンではなく角度(°)
-        angle = 45.0
+        angle = 10.0
         # 拡大比率
         scale = 1.0
         # 回転変換行列の算出
@@ -101,7 +101,7 @@ for idx1, jp_font in enumerate(jp_font_list):
         affine_image = Image.fromarray(blur_rot)
 
         # 画像ファイルを保存する
-        affine_image.save("./image/" + place_name + "/" + str(idx1) + "_GaussianBlur_Affine45_" + place_name + '.png', 'PNG')
+        affine_image.save("./image/" + place_name + "/" + str(idx1) + "_GaussianBlur_Affine1_" + place_name + '.png', 'PNG')
         data = np.asarray(affine_image)        
         X.append(data.astype(np.float64))
         Y.append(idx2)
@@ -110,15 +110,15 @@ for idx1, jp_font in enumerate(jp_font_list):
 for idx1, jp_font in enumerate(jp_font_list):
     # フォントの指定。引数は順に「フォントのパス」「フォントのサイズ」「エンコード」
     # メソッド名からも分かるようにTure Typeのフォントを指定する
-    font = ImageFont.truetype(jp_font, 30)
+    font = ImageFont.truetype(jp_font, 20)
 
     for idx2, place_name in enumerate(place_list):
         # 画像のピクセルを指定
-        image = Image.new('L', (300, 300))
+        image = Image.new('RGB', (300, 300), 'blue')
         draw = ImageDraw.Draw(image)
         # 日本語の文字を入れてみる
         # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
-        draw.text((150, 150), place_name, font=font, fill='#ffffff')
+        draw.text((25, 45), place_name, font=font, fill='#ffffff')
         # opencvのガウシアンフィルターを適応
         image_np = np.asarray(image)
         blur = cv2.GaussianBlur(image_np, (5, 5), 0)
@@ -129,7 +129,7 @@ for idx1, jp_font in enumerate(jp_font_list):
         size = tuple(np.array([blur.shape[1], blur.shape[0]]))
         # 回転させたい角度
         # ラジアンではなく角度(°)
-        angle = 320.0
+        angle = 15.0
         # 拡大比率
         scale = 1.0
         # 回転変換行列の算出
@@ -139,7 +139,45 @@ for idx1, jp_font in enumerate(jp_font_list):
         affine_image = Image.fromarray(blur_rot)
 
         # 画像ファイルを保存する
-        affine_image.save("./image/" + place_name + "/" + str(idx1) + "_GaussianBlur_Affine320_" + place_name + '.png', 'PNG')
+        affine_image.save("./image/" + place_name + "/" + str(idx1) + "_GaussianBlur_Affine2_" + place_name + '.png', 'PNG')
+        data = np.asarray(affine_image)        
+        X.append(data.astype(np.float64))
+        Y.append(idx2)
+
+# ４、ガウシアンフィルタでぼかしつつ、アフィン変換で画面を回転させる
+for idx1, jp_font in enumerate(jp_font_list):
+    # フォントの指定。引数は順に「フォントのパス」「フォントのサイズ」「エンコード」
+    # メソッド名からも分かるようにTure Typeのフォントを指定する
+    font = ImageFont.truetype(jp_font, 20)
+
+    for idx2, place_name in enumerate(place_list):
+        # 画像のピクセルを指定
+        image = Image.new('RGB', (300, 300), 'blue')
+        draw = ImageDraw.Draw(image)
+        # 日本語の文字を入れてみる
+        # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
+        draw.text((25, 45), place_name, font=font, fill='#ffffff')
+        # opencvのガウシアンフィルターを適応
+        image_np = np.asarray(image)
+        blur = cv2.GaussianBlur(image_np, (5, 5), 0)
+        # 画像の中心位置
+        # 今回は画像サイズの中心をとっている
+        center = tuple(np.array([blur.shape[1] * 0.5, blur.shape[0] * 0.5]))
+        # 画像サイズの取得(横, 縦)
+        size = tuple(np.array([blur.shape[1], blur.shape[0]]))
+        # 回転させたい角度
+        # ラジアンではなく角度(°)
+        angle = 15.0
+        # 拡大比率
+        scale = 1.0
+        # 回転変換行列の算出
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
+        # アフィン変換
+        blur_rot = cv2.warpAffine(blur, rotation_matrix, size, flags=cv2.INTER_CUBIC)
+        affine_image = Image.fromarray(blur_rot)
+
+        # 画像ファイルを保存する
+        affine_image.save("./image/" + place_name + "/" + str(idx1) + "_GaussianBlur_Affine3_" + place_name + '.png', 'PNG')
         data = np.asarray(affine_image)        
         X.append(data.astype(np.float64))
         Y.append(idx2)
