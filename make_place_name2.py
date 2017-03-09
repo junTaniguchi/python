9184d53d91cb360990f43969aef4c3ec054f6cf3
junTaniguchi <junTaniguchi@users.noreply.github.com>
2017-03-09 16:48:47 +0900

    Update make_place_name2.py

--- make_place_name2.py
+++ make_place_name2.py
@@ -1,6 +1,7 @@
 # -*- encoding:utf8 -*-
 import os
 from PIL import Image, ImageDraw, ImageFont
+import cv2
 import numpy as np
 
 path = "/Users/j13-taniguchi/study_tensorflow/keras_project/read_place"
@@ -24,42 +25,124 @@ for place_name in place_list:
 
 X = []
 Y = []
+# １、フォントの標準文字
 for idx1, jp_font in enumerate(jp_font_list):
     # フォントの指定。引数は順に「フォントのパス」「フォントのサイズ」「エンコード」
     # メソッド名からも分かるようにTure Typeのフォントを指定する
-    font = ImageFont.truetype(jp_font, 20, encoding='unic')
+    font = ImageFont.truetype(jp_font, 20)
 
     for idx2, place_name in enumerate(place_list):
         # 画像のピクセルを指定
-        image = Image.new('RGB', (300, 300), "green")
+        image = Image.new('L', (300, 300))
         draw = ImageDraw.Draw(image)
         # 日本語の文字を入れてみる
         # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
         draw.text((25, 45), place_name, font=font, fill='#ffffff')
         # 画像ファイルを保存する
-        image.save("./image/" + place_name + "/" + str(idx1) + "_upperleft_" + place_name + '.png', 'PNG')
+        image.save("./image/" + place_name + "/" + str(idx1) + "_" + place_name + '.png', 'PNG')
         data = np.asarray(image)        
-        X.append(data)
+        X.append(data.astype(np.float64))
         Y.append(idx2)
 
+# ２、ガウシアンフィルタでぼかす
 for idx1, jp_font in enumerate(jp_font_list):
     # フォントの指定。引数は順に「フォントのパス」「フォントのサイズ」「エンコード」
     # メソッド名からも分かるようにTure Typeのフォントを指定する
-    font = ImageFont.truetype(jp_font, 30, encoding='unic')
+    font = ImageFont.truetype(jp_font, 50)
 
     for idx2, place_name in enumerate(place_list):
         # 画像のピクセルを指定
-        image = Image.new('RGB', (300, 300), "blue")
+        image = Image.new('L', (300, 300))
         draw = ImageDraw.Draw(image)
         # 日本語の文字を入れてみる
         # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
-        draw.text((170, 150), place_name, font=font, fill='#ffffff')
+        draw.text((140, 150), place_name, font=font, fill='#ffffff')
+        # opencvのガウシアンフィルターを適応
+        image_np = np.asarray(image)
+        blur = cv2.GaussianBlur(image_np, (5, 5), 0)
+        blur_image = Image.fromarray(blur)
+        
         # 画像ファイルを保存する
-        image.save("./image/" + place_name + "/" + str(idx1) + "_lowerright_" + place_name + '.png', 'PNG')
-        data = np.asarray(image)        
-        X.append(data)
+        image.save("./image/" + place_name + "/" + str(idx1) + "_GaussianBlur_" + place_name + '.png', 'PNG')
+        data = np.asarray(blur_image)       
+        X.append(data.astype(np.float64))
         Y.append(idx2)
 
+# ３、ガウシアンフィルタでぼかしつつ、アフィン変換で画面を回転させる
+for idx1, jp_font in enumerate(jp_font_list):
+    # フォントの指定。引数は順に「フォントのパス」「フォントのサイズ」「エンコード」
+    # メソッド名からも分かるようにTure Typeのフォントを指定する
+    font = ImageFont.truetype(jp_font, 40)
+
+    for idx2, place_name in enumerate(place_list):
+        # 画像のピクセルを指定
+        image = Image.new('L', (300, 300))
+        draw = ImageDraw.Draw(image)
+        # 日本語の文字を入れてみる
+        # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
+        draw.text((100, 150), place_name, font=font, fill='#ffffff')
+        # opencvのガウシアンフィルターを適応
+        image_np = np.asarray(image)
+        blur = cv2.GaussianBlur(image_np, (5, 5), 0)
+        # 画像の中心位置
+        # 今回は画像サイズの中心をとっている
+        center = tuple(np.array([blur.shape[1] * 0.5, blur.shape[0] * 0.5]))
+        # 画像サイズの取得(横, 縦)
+        size = tuple(np.array([blur.shape[1], blur.shape[0]]))
+        # 回転させたい角度
+        # ラジアンではなく角度(°)
+        angle = 45.0
+        # 拡大比率
+        scale = 1.0
+        # 回転変換行列の算出
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
+        # アフィン変換
+        blur_rot = cv2.warpAffine(blur, rotation_matrix, size, flags=cv2.INTER_CUBIC)
+        affine_image = Image.fromarray(blur_rot)
+
+        # 画像ファイルを保存する
+        affine_image.save("./image/" + place_name + "/" + str(idx1) + "_GaussianBlur_Affine45_" + place_name + '.png', 'PNG')
+        data = np.asarray(affine_image)        
+        X.append(data.astype(np.float64))
+        Y.append(idx2)
+
+# ４、ガウシアンフィルタでぼかしつつ、アフィン変換で画面を回転させる
+for idx1, jp_font in enumerate(jp_font_list):
+    # フォントの指定。引数は順に「フォントのパス」「フォントのサイズ」「エンコード」
+    # メソッド名からも分かるようにTure Typeのフォントを指定する
+    font = ImageFont.truetype(jp_font, 30)
+
+    for idx2, place_name in enumerate(place_list):
+        # 画像のピクセルを指定
+        image = Image.new('L', (300, 300))
+        draw = ImageDraw.Draw(image)
+        # 日本語の文字を入れてみる
+        # 引数は順に「(文字列の左上のx座標, 文字列の左上のy座標)」「フォントの指定」「文字色」
+        draw.text((150, 150), place_name, font=font, fill='#ffffff')
+        # opencvのガウシアンフィルターを適応
+        image_np = np.asarray(image)
+        blur = cv2.GaussianBlur(image_np, (5, 5), 0)
+        # 画像の中心位置
+        # 今回は画像サイズの中心をとっている
+        center = tuple(np.array([blur.shape[1] * 0.5, blur.shape[0] * 0.5]))
+        # 画像サイズの取得(横, 縦)
+        size = tuple(np.array([blur.shape[1], blur.shape[0]]))
+        # 回転させたい角度
+        # ラジアンではなく角度(°)
+        angle = 320.0
+        # 拡大比率
+        scale = 1.0
+        # 回転変換行列の算出
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
+        # アフィン変換
+        blur_rot = cv2.warpAffine(blur, rotation_matrix, size, flags=cv2.INTER_CUBIC)
+        affine_image = Image.fromarray(blur_rot)
+
+        # 画像ファイルを保存する
+        affine_image.save("./image/" + place_name + "/" + str(idx1) + "_GaussianBlur_Affine320_" + place_name + '.png', 'PNG')
+        data = np.asarray(affine_image)        
+        X.append(data.astype(np.float64))
+        Y.append(idx2)
 
 X = np.array(X)
 Y = np.array(Y)
