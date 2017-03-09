380eed061e2708718a6d8365b55f1b4ceb49e4ac
junTaniguchi <junTaniguchi@users.noreply.github.com>
2017-03-09 16:49:19 +0900

    Update learning_place_name.py

--- learning_place_name.py
+++ learning_place_name.py
@@ -4,6 +4,7 @@ Created on Tue Mar  7 16:45:08 2017
 @author: j13-taniguchi
 """
 import os
+from keras.models import Sequential
 import tensorflow as tf
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
@@ -15,6 +16,8 @@ import keras.callbacks
 import keras.backend.tensorflow_backend as KTF
 from keras.utils.visualize_util import plot
 import json
+from PIL import Image, ImageDraw, ImageFont
+
 
 image_w = 300
 image_h = 300
@@ -30,32 +33,31 @@ xy = np.load("./param/place_name.npz")
 X = xy["x"]
 Y = xy["y"]
 # データを正規化
-X = X.reshape(X.shape[0], X.shape[1], X.shape[2], X.shape[3]).astype('float32')
 X /= 255
 
 #地名のリストを作成
 with open("./param/place_tokyo.txt", "r") as place_file:
     place_list = place_file.readlines()
     place_list = [place_str.strip() for place_str in place_list]
-    NUM_CLASSES = len(place_list)
-
+NUM_CLASSES = len(place_list)
 Y = np_utils.to_categorical(Y, NUM_CLASSES)
 
 # 訓練データとテストデータに分割
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y)
-X_train = np.reshape(X_train, (len(X_train),  300, 300, 3))
-X_test  = np.reshape(X_test, (len(X_test), 300, 300, 3))
+X_train = np.reshape(X_train, (len(X_train),  300, 300, 1))
+X_test  = np.reshape(X_test, (len(X_test), 300, 300, 1))
 print('X_train shape:', X_train.shape)
 
 #VGG
-old_session = KTF.get_session()
+#old_session = KTF.get_session()
 
 with tf.Graph().as_default():
+    '''
     session = tf.Session('')
     KTF.set_session(session)
     KTF.set_learning_phase(1)
     # モデルを構築
-    model = SSD300(input_shape=(300, 300, 3), num_classes=NUM_CLASSES)
+    model = SSD300(input_shape=(300, 300, 1), num_classes=NUM_CLASSES)
     model.compile(loss='categorical_crossentropy',
         optimizer=RMSprop(),
         metrics=['accuracy'])
@@ -74,11 +76,67 @@ with tf.Graph().as_default():
                         nb_epoch=1,
                         verbose=1,
                         validation_data=(X_test, y_test))
-    plot(model, to_file='learning_japanese2.png')
+    '''
+    session = tf.Session('')
+    KTF.set_session(session)
+    KTF.set_learning_phase(1)
+    # モデルを構築
+    model = Sequential()
+    model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(image_w, image_h, 1)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+        optimizer=RMSprop(),
+        metrics=['accuracy'])
+    model.summary()
+    
+    # callback関数にて収束判定を追加    
+    keras.callbacks.EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
+    # callback関数にてTensorboardを可視化
+    tb_cb = keras.callbacks.TensorBoard(log_dir=log_filepath,
+                                        histogram_freq=1,
+                                        write_graph=True)
+    cbks = [tb_cb]
+    if os.path.exists('learning_place_name.hdf5'):
+        model.load_weights('learning_place_name.hdf5', by_name=True)
+    # 学習開始
+    history = model.fit(X_train, y_train,
+                        batch_size=128,
+                        nb_epoch=50,
+                        verbose=1,
+                        validation_data=(X_test, y_test))
+    
+
+    plot(model, to_file='learning_place_name.png')
     # モデルを保存
-    model.save_weights('japanese_dataset.hdf5')
+    model.save_weights('learning_place_name.hdf5')
     model_json = model.to_json()
-    with open('japanese_dataset.json', 'w') as json_file:
+    with open('learning_place_name.json', 'w') as json_file:
         json.dump(model_json, json_file, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
     
     # モデルを評価
@@ -86,4 +144,33 @@ with tf.Graph().as_default():
     print('Test score:', score[0])
     print('Test accuracy;', score[1])
 
-KTF.set_session(old_session)
+#KTF.set_session(old_session)
+print("finish!!")
+
+# 予測
+result_X_test = model.predict(X_test)
+# 予測結果と正解のラベルを照合する
+for idx, idx_result_X in enumerate(result_X_test):
+    # 予測結果のargmaxを抽出    
+    result_idx = idx_result_X.argmax()
+    result_label = place_list[result_idx]
+    # 正解の番号を抽出
+    answer_idx = y_test[idx].argmax()
+    answer_label = place_list[answer_idx]
+    # 予測結果と正解の値を比較
+    if result_idx == answer_idx:
+        continue
+    # 不正解をコンソールへ表示   
+    error_message = "incorrect: answer: %s result: %s" %(answer_label, result_label)    
+    print(error_message)
+
+    # 不正解の画像をincorrectディレクトリへ格納する準備
+    incorrect_dir = "./incorrect/%s/" %(answer_label)
+    if not os.path.exists(incorrect_dir):
+        os.makedirs(incorrect_dir)
+    incorrect_file_name = incorrect_dir + answer_label + ".png"
+    # 不正解だったデータを画像化
+    # idx_result_Xを非正規化
+    idx_result_X *= 256
+    img = Image.fromarray(np.uint8(idx_result_X))
+    img.save(incorrect_file_name)    
